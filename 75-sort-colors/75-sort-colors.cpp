e30d99edfcbf209f6a037d24225e40fd8846c156
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-03 17:11:25 +0530

    Time: 8 ms (11.74%), Space: 8.3 MB (70.37%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,24 +1,15 @@
 class Solution {
+    class Compare{
+        public:
+          bool operator()(int a,int b){
+              return a>b;
+          }
+    };
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 0){
-                ans.push_back(0);
-            }
+         make_heap(nums.begin(),nums.end());
+        for(int i =0; i < nums.size(); i++){
+            pop_heap(nums.begin(),nums.end()-i);
         }
-         for(int i=0;i<nums.size();i++){
-            if(nums[i] == 1){
-                ans.push_back(1);
-            }
-        }
-         for(int i=0;i<nums.size();i++){
-            if(nums[i] == 2){
-                ans.push_back(2);
-            }
-        }
-        nums = ans;
-        
-        
     }
 };
