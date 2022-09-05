9071522e01d1c22e9d29355a30a26457629fc224
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-05 20:19:06 +0530

    Time: 33 ms (10.55%), Space: 6 MB (25.44%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 1 || x == 0) return x;
+        
+        long int i =0;
+        for(i = 2; i*i <= x;i++){
+            
+        }
+        return i-1;
+    }
+};
