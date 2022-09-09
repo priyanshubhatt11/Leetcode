8a710111505fcdcca17655588864fedd3b88f8c4
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-09 14:18:00 +0530

    Time: 288 ms (7.86%), Space: 21.1 MB (6.90%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -21,7 +21,7 @@ public:
     int maximalSquare(vector<vector<char>>& mat) {
         int ret =0,ans =0;
         
-        vector<vector<int>> dp(mat.size()+1, vector<int> (mat[0].size()+1, -1));
+        vector<vector<int>> dp(mat.size(), vector<int> (mat[0].size(), -1));
         
         for(int i=0;i<mat.size();i++){
             for(int j=0;j<mat[0].size();j++){
