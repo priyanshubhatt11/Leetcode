cb024001f413259fa48f7a34f1826b5874ff4e98
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-10 13:54:43 +0530

    Time: 61 ms (10.59%), Space: 23.1 MB (12.87%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -11,12 +11,39 @@ public:
         return dp[idx][k] = ans;
     }
     
+    int solveTab(vector<int> nums){
+        int n = nums.size();
+        vector<vector<int>> dp1(n+2 , vector<int>(n, 0));
+        vector<vector<int>> dp2(n+2 , vector<int>(n, 0));
+        
+        for(int idx = n-2; idx>=0;idx--){
+            for(int k = 1; k<=n/3;k++ ){
+                int inc = nums[idx] + dp1[idx+2][k-1];  
+                int exc = dp1[idx+1][k];
+                
+                dp1[idx][k] = max(inc, exc);
+            }
+        }
+        for(int idx = n-1; idx>=1;idx--){
+            for(int k = 1; k<=n/3;k++){
+                int inc = nums[idx] + dp2[idx+2][k-1];  
+                int exc = dp2[idx+1][k];
+                dp2[idx][k] = max(inc, exc);
+            }
+        }
+        return max(dp1[0][n/3],dp2[1][n/3] );
+        
+    }
+    
     int maxSizeSlices(vector<int>& slices) {
         int k = slices.size();
-        vector<vector<int>> dp1(k , vector<int>(k, -1));
-        vector<vector<int>> dp2(k , vector<int>(k, -1));
-        int ans1 = solve(slices, 0, k-2 ,k/3, dp1);
-        int ans2 = solve(slices, 1, k-1, k/3, dp2);
-        return max(ans1, ans2);
+        // vector<vector<int>> dp1(k , vector<int>(k, -1));
+        // vector<vector<int>> dp2(k , vector<int>(k, -1));
+        // int ans1 = solve(slices, 0, k-2 ,k/3, dp1);
+        // int ans2 = solve(slices, 1, k-1, k/3, dp2);
+        
+        // return max(ans1, ans2);
+        
+        return solveTab(slices);
     }
 };
