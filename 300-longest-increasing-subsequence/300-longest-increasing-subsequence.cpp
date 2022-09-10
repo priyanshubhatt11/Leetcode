a79556da7721bc2ac497d2a91634a8eee924b2bc
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-10 10:33:37 +0530

    Time: 952 ms (15.37%), Space: 292.6 MB (8.64%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,41 +1,7 @@
 class Solution {
 public:
     
-    int solve(vector<int> &nums, int n, int cur, int prev, vector<vector<int>>&dp){
-        if(cur == n)return 0;
-        
-        if(dp[cur][prev+1] != -1)return dp[cur][prev+1]; 
-        
-        int include =0;
-        if(prev == -1 || nums[cur] > nums[prev]){
-            include = 1 + solve(nums,n, cur+1, cur, dp);
-        }
-        int exclude = solve(nums, n, cur + 1, prev, dp);
-        
-        return dp[cur][prev+1] = max(include, exclude);
-        
-        
-    }
-    
-    int solveTab(vector<int> &num){
-        int n = num.size();
-        vector<vector<int>>dp (n+1, vector<int> (n+1, 0));
-        
-        for(int  cur= n-1; cur>=0; cur--){
-            for(int prev = cur-1;prev >= -1; prev--){
-                
-                int include = 0;
-                if(prev == -1 || num[cur] > num[prev]){
-                    include = 1 + dp[cur+1][cur+1];
-                } 
-                int exclude = dp[cur+1][prev+1];
-                
-                dp[cur][prev+1] = max(include, exclude);
-            }
-        }
-        return dp[0][0];
-        
-    }
+
     
         int solveSpaceOpt(vector<int> &num){
         int n = num.size();
