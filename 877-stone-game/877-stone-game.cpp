e440cfa3e40f1456f7dae46bb34150d522110dcc
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-16 16:31:02 +0530

    Time: 10 ms (49.02%), Space: 7.6 MB (70.01%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    
-    int solve(vector<int>&pile, int i, int j){
-        if(i>j)return 0;
-        
-        return max(pile[i] - solve(pile, i+1, j) , pile[j] - solve(pile, i, j-1));
-    }
-    
-    bool stoneGame(vector<int>& p) {
-        // int ans = solve(p, 0, p.size()-1);
-        // cout<<ans;
+    bool stoneGame(vector<int>& piles) {
         return 1;
     }
 };
