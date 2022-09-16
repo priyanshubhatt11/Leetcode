b8656a34a0fa36e1f41cf353b91cfc13b1ed7d01
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-16 16:36:53 +0530

    Time: 49 ms (19.22%), Space: 16.5 MB (24.34%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
+    
+    int solve(int i, int j, vector<int> &pile, vector<vector<int>>&dp){
+        if(i>j)return 0;
+        if(dp[i][j] != INT_MIN)return dp[i][j];
+        
+        return dp[i][j] = max(pile[i] - solve(i+1,j,pile, dp), pile[j] - solve(i, j-1, pile, dp));
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        return 1;
+        // return 1; // Answer will be always true
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        int ans = solve(0, n-1, piles, dp);
+        if(ans >0)return true;
+        return false;
     }
 };
