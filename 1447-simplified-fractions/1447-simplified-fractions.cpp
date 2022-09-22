524b480bbdeb49a6acbe8217e7a427ac06ccd382
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-22 17:26:28 +0530

    Time: 90 ms (68.90%), Space: 21.8 MB (98.51%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.cpp
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    
+    int findgcd(int n,int m){
+        while(n!=m){
+            if(n>m) n=n-m;
+            else m=m-n;
+        }
+        return n;
+    }
+    
     vector<string> simplifiedFractions(int n) {
         vector<string> ans;
         for(int i=1;i<n;i++){
             for(int j= i+1;j<=n;j++){
                 
-               if(gcd(i,j) == 1){
+               if(findgcd(i,j) == 1){
                 
                     string nn = "";
                     nn = to_string(i) + "/" + to_string(j);
