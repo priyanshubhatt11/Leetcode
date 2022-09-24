19681a67da085800db355d10913df74e18d5c39b
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-24 11:08:49 +0530

    Time: 6 ms (6.24%), Space: 6 MB (18.92%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.cpp
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.cpp
@@ -4,12 +4,11 @@ public:
         string hex="";
         vector<char> digits ={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
         
-        while(num){
+        while(num!=0){
             hex.push_back(digits[(0xf & num)]);
             num=num>>4;
         }       
         reverse(hex.begin(),hex.end());
-        //reverse(ans.begin(), ans.end());
         if(hex == "")return "0";
         return hex;
     }
