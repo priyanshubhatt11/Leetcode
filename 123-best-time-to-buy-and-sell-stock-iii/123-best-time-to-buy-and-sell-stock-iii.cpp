403386334116896b2911c728e62a223e19eef5a4
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-28 18:57:45 +0530

    Time: 2019 ms (5.02%), Space: 283.7 MB (5.00%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     
-    int solve(vector<int> &prices, int idx,int mm, bool buy, vector<vector<vector<int>>> &dp){
-        if(idx >= prices.size())return 0;
-        if(mm == 0)return 0;
-        if(dp[idx][buy][mm] != -1)return dp[idx][buy][mm];
+    int solve(vector<int>&prices, int idx, int m, bool buy, vector<vector<vector<int>>> &dp){
+        if(idx>=prices.size())return 0;
+        if(m == 0)return 0;
+        if(dp[idx][m][buy] != -1)return dp[idx][m][buy];
         
         int profit =0;
         if(buy){
-            int buyi = -prices[idx] + solve(prices, idx+1, mm, false, dp);
-            int notbuyi = solve(prices, idx+1, mm, true, dp);
-            profit = max(buyi, notbuyi);
+            int buyit = -prices[idx] + solve(prices, idx+1, m, false, dp);
+            int dontbuy = solve(prices, idx+1, m, true, dp);
+            profit = max(buyit, dontbuy);
         }
         else{
-            int selli = prices[idx] + solve(prices, idx+1, mm-1, true, dp);
-            int notselli = solve(prices, idx+1, mm, false, dp);
-            profit = max(selli, notselli);
+            int sellit = prices[idx] + solve(prices, idx+1, m-1, true, dp);
+            int dontsell = solve(prices, idx+1, m, false, dp);
+            profit = max(sellit, dontsell);
         }
-        return dp[idx][buy][mm] = profit;
-        
+        return dp[idx][m][buy] = profit;
         
     }
     
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n , vector<vector<int>> (2, vector<int> (3, -1)));
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>> (3, vector<int>(2, -1)));
         return solve(prices, 0, 2, true, dp);
     }
 };
