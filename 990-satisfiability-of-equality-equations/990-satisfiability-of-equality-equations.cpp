a603d789809fe93c6c79a0c6777020d0450bf0e6
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-09-28 22:25:14 +0530

    Time: 9 ms (67.10%), Space: 11.3 MB (62.96%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,17 +1,25 @@
 class Solution {
-    int uf[26];
-    int find(int x) {
-        return uf[x] == x ? x : (uf[x] = find(uf[x]));
-    }
 public:
-    bool equationsPossible(vector<string>& equations) {
-        for (int i = 0; i < 26; ++i) uf[i] = i;
-        for (auto e : equations) {
-            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a'); 
+    int par[26];
+    
+    int find(int x){
+        if(par[x] == x)return x;
+        return par[x] = find(par[x]);
+    }
+    bool equationsPossible(vector<string>& eq) {
+        for(int i=0;i<26;i++){
+            par[i] = i;
         }
-        for (auto e : equations) {
-            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
+        
+        for(auto i: eq){
+            if(i[1] == '='){
+                par[find(i[3] - 'a')] = find(i[0]- 'a');
+            }
         }
+        for(auto i:eq){
+            if(i[1] == '!' && find(i[0] - 'a') == find(i[3] - 'a'))return false;
+        }
+        
         return true;
     }
 };
