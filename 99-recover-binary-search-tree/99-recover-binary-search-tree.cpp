fc1bccab3ec68ea1d29d80dcc32e8aacb087d235
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-06 00:17:49 +0530

    Time: 73 ms (24.11%), Space: 57.8 MB (70.20%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
-    int i=0;
-    void inorder(TreeNode *root, vector<int>&vec){
-        if(root == NULL)return;
-        
-        inorder(root->left, vec);
-        vec.push_back(root->val);
-        inorder(root->right, vec);
-    }
     
-    void inorderInsert(TreeNode *root, vector<int>&vec){
-        if(root == NULL)return;
-        inorderInsert(root->left, vec);
-        if(vec[i] != root->val){
-            root->val = vec[i];
-        }
-        i++;
-        inorderInsert(root->right, vec);
+    
+    TreeNode *prev = NULL, *first = NULL, *second = NULL;
+    
+//     int i=0;
+//     void inorder(TreeNode *root, vector<int>&vec){
+//         if(root == NULL)return;
+        
+//         inorder(root->left, vec);
+//         vec.push_back(root->val);
+//         inorder(root->right, vec);
+//     }
+    
+//     void inorderInsert(TreeNode *root, vector<int>&vec){
+//         if(root == NULL)return;
+//         inorderInsert(root->left, vec);
+//         if(vec[i] != root->val){
+//             root->val = vec[i];
+//         }
+//         i++;
+//         inorderInsert(root->right, vec);
         
        
-    }
-    void recoverTree(TreeNode* root) {
-        vector<int> vec;
-        inorder(root,vec);
-        sort(vec.begin(), vec.end());
+//     }
+    
+    void inorder2(TreeNode * root){
+        if(root == NULL)return;
         
-        inorderInsert(root,  vec);
+        
+        inorder2(root->left);
+        
+        if(prev!=NULL && prev->val > root->val){
+            if(first == NULL)   first = prev;
+            
+            second = root;
+        }
+        prev = root;
+        
+        inorder2(root->right);
+        
+    }
+    
+    void recoverTree(TreeNode* root) {
+//         vector<int> vec;
+//         inorder(root,vec);
+//         sort(vec.begin(), vec.end());
+        
+//         inorderInsert(root,  vec);
+        
+        
+        inorder2(root);
+        swap(first->val, second->val);
     }
 };
