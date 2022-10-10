d5f8f3464c277dccd33d44221faf1cac6de580c2
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-10 19:41:32 +0530

    Time: 117 ms (54.42%), Space: 45 MB (69.89%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,99 +1,60 @@
-class TrieNode{
-    public:
-    char data;
-    TrieNode *child[26];
-    bool isEnd;
-    TrieNode(char ch){
-        data = ch;
-        for(int i=0;i<26;i++){
-            child[i] = NULL;
-        }
-        isEnd = false;
-    }
-};
-
 class Trie {
-public:
-    TrieNode* root;
-    Trie() {    
-        root = new TrieNode('\0');
-    }
-    
-    /***********************************/
-    void insertUtil(TrieNode* root, string word){
-        
-        if(word.length() == 0){
-            root->isEnd = true;
-            return;
-        }
-        
-        TrieNode* temp;
-        int idx = word[0] - 'a';
-        if(root->child[idx] != NULL){
-            temp = root->child[idx];
-        }
-        else{
-            temp = new TrieNode(word[0]);
-            root->child[idx] = temp;
-        }
-        insertUtil(temp , word.substr(1));
-        
+    class TrieNode{
+            public:
+            TrieNode* child[26];
+            bool isEnd;
+            TrieNode(){
+                for(int i=0;i<26;i++){
+                    child[i] = NULL;
+                }
+                isEnd = false;
+            }
+        };
+    public:
+    TrieNode *root;
+    Trie() {
+        root = new TrieNode();        
     }
     
     void insert(string word) {
-        insertUtil(root, word); 
+        TrieNode* temp = root;
+        for(int i=0;i<word.size();i++){
+            int idx = word[i] - 'a';
+            if(temp->child[idx] == NULL){
+                temp->child[idx] = new TrieNode();
+            }
+            temp = temp->child[idx];
+        }
+        temp->isEnd = true;
     }
-    /*************************************/
     
-    bool searchUtil(TrieNode *root, string word){
-        //cout<<word<<" ";
-        if(word.length() == 0){
-            return root->isEnd;
-        }
-        
-        TrieNode *temp;
-        int idx = word[0] - 'a';
-        if(root->child[idx] != NULL){
-            temp = root->child[idx];
-        }        
-        else{
-            return false;
-        }
-        
-        return searchUtil(temp, word.substr(1));
-        
-    }
     bool search(string word) {
-        return searchUtil(root, word);
+        TrieNode* temp = root;
+        for(int i=0;i<word.size();i++){
+            int idx = word[i] - 'a';
+            if(temp->child[idx] == NULL)return false;
+            
+            temp = temp->child[idx];
+        }
+        return temp->isEnd;
     }
     
-    /**************************************/
-    bool startsWithUtil(TrieNode *root, string pre){
-        if(pre.length() == 0){
-            return true;
+    bool startsWith(string pre) {
+        TrieNode *temp = root;
+        for(int i=0;i<pre.size();i++){
+            
+            int idx = pre[i] - 'a';
+            if(temp->child[idx] == NULL)return false;
+            temp = temp->child[idx];
         }
-        TrieNode *temp;
-        int idx = pre[0] - 'a';
-        if(root->child[idx] != NULL){
-            temp = root->child[idx];
-        }
-        else{
-            return false;
-        }
-        return startsWithUtil(temp , pre.substr(1));
+        return true;
     }
-    
-    bool startsWith(string prefix) {
-        return startsWithUtil(root, prefix);
-    }
-    /*************************************/
 };
 
 
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
+
+
+
+
+
+
