db77e862a29c62c0462e5c338c912d43c340647c
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-11 10:41:36 +0530

    Time: 155 ms (18.33%), Space: 26.4 MB (8.61%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,19 +1,71 @@
+class Trie{
+    public:
+    Trie *child[10];
+    bool isEnd;
+    Trie(){
+        for(int i=0;i<10;i++){
+            child[i] = NULL;
+        }
+        isEnd = false;
+    }
+};
+
 class Solution {
 public:
     
-    void solve(int i, int n, vector<int>&vec){
-        if(i > n)return;
-        vec.push_back(i);
-        for(int j=0;j<=9;j++){
-            solve(i *10 + j, n, vec);
+    Trie *root;
+    Solution(){
+        root = new Trie();
+    }
+    
+    void insert(Trie *root, int n){
+        Trie *temp = root;
+        string s = to_string(n);
+        for(int i=0;i<s.size();i++){
+            int idx = s[i] - '0';
+            if(temp->child[idx] == NULL){
+                temp->child[idx] = new Trie();
+            }
+            temp = temp->child[idx];
+        }
+        temp->isEnd = true;
+    }
+    
+    void solve(Trie *root, vector<int> &ans, string s){
+        if(root == NULL)return;
+        
+        if(root->isEnd == true){
+            //cout<s<<" ";
+            ans.push_back(stoi(s));
+        }
+        
+        for(int i=0;i<10;i++){
+            if(root->child[i] != NULL){
+                
+                solve(root->child[i], ans,s+to_string(i));
+            }
         }
     }
     
+    
     vector<int> lexicalOrder(int n) {
-        vector<int> vec;
-        for(int i=1;i<=9;i++){
-            solve(i, n, vec);
+        for(int i=1;i<=n;i++){
+            insert(root, i);
         }
-        return vec;
+        vector<int> ans;
+        solve(root, ans, "");
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
