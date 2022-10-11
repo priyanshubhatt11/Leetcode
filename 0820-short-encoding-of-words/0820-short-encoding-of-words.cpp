bb5a5facc8b7a32517de5b73bfa20491f710d449
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-11 16:19:19 +0530

    Time: 237 ms (29.32%), Space: 74 MB (21.17%) - LeetHub

---
+++ 0820-short-encoding-of-words/0820-short-encoding-of-words.cpp
@@ -0,0 +1,67 @@
+class Trie{
+    public:
+    Trie* child[26];
+    bool isEnd;
+    Trie(){
+        for(int i=0;i<26;i++){
+            child[i] = NULL;
+        }
+        isEnd = false;
+    }
+};
+
+class Solution {
+public:
+    
+    Trie *root;
+    Solution(){
+        root = new Trie();
+    }
+    
+    void insert(string s){
+        Trie *temp = root;  
+        for(int i=s.size()-1;i>=0;i--){        // Inserted in reverse order
+            int idx = s[i] - 'a';
+            if(temp->child[idx] == NULL){
+                temp->child[idx] = new Trie();
+            }
+            temp = temp->child[idx];
+        }
+        temp->isEnd = true;
+    }
+    
+    int ans=0;
+    void findd(Trie *root, int c){
+        bool last = false;  // to check the end
+        for(int i=0; i<26;i++){
+            if(root->child[i] != NULL){
+                cout<<(char)(i+'a');
+                findd(root->child[i] , c+1);
+                last = true;
+            }
+        }
+        if(last == false){
+            ans+= c+1;
+        }
+        
+    }
+        
+    int minimumLengthEncoding(vector<string>& words) {
+        for(auto i:words){
+            insert(i);      
+        }   
+        
+        findd(root, 0);
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
