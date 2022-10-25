ea2660ceb5cdb2af6280960dace04b7e88417f76
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-25 17:38:34 +0530

    Time: 464 ms (57.16%), Space: 86.7 MB (82.73%) - LeetHub

---
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    //  odd can only become odd and even can only become even because operation is +-2
+    long long makeSimilar(vector<int>& nums, vector<int>& tar) {
+        sort(nums.begin(), nums.end());
+        sort(tar.begin(), tar.end());
+        
+        vector<int> odd, even, oddtar, eventar;
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2 == 0)even.push_back(nums[i]);
+            else    odd.push_back(nums[i]);
+            
+            if(tar[i]%2 == 0)eventar.push_back(tar[i]);
+            else    oddtar.push_back(tar[i]);
+        }
+        
+        long long ans1 =0, ans2 =0;
+        
+        for(int i=0;i<even.size();i++){
+            ans1 += abs(even[i] - eventar[i])/2;
+        }
+        for(int i=0;i<odd.size();i++){
+            ans2 += abs(odd[i] - oddtar[i])/2;
+        }
+        
+        return (ans1+ans2)/2;
+    }
+};
