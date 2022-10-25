4186d1e8eefb21821d09d471f90878c3f1d382e1
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-25 08:58:26 +0530

    Time: 88 ms (100.00%), Space: 43.1 MB (100.00%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,33 +1,27 @@
-//  https://www.youtube.com/watch?v=MbAhaBBOmQc
-
-// in example 1 - [1 ,1 ,3 ,3 ,3 ,5 ,2 ,2 ,2 ,2 ,2 ,2 ,2, 2, 2, 2, 2, 2, 2, 2 ]; ans then find median
 class Solution {
 public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
         int n = nums.size();
         vector<pair<int,int>> vec;
-        
         for(int i=0;i<n;i++){
             vec.push_back({nums[i], cost[i]});
         }
         sort(vec.begin(), vec.end());
         
-        vector<long long> prefS(n, 0);
-        prefS[0] = vec[0].second;
-        for(int i=1;i<n;i++){
-            prefS[i] += vec[i].second + prefS[i-1];
-        }
+        vector<long long> pref(n, 0);
+        pref[0] = vec[0].second;
         
-        long long x = prefS[n-1]/2;   // median
-        //cout<<x<<" ";
+        for(int i=1;i<n;i++){
+            pref[i] = pref[i-1] + vec[i].second;
+        }
+        long long x = pref[n-1]/2;
         int tar =0;
         for(int i=0;i<n;i++){
-            if(prefS[i] >= x){
+            if(pref[i] >= x){
                 tar = i;
                 break;
             }
         }
-        //cout<<tar<<" ";
         long long ans =0;
         for(int i=0;i<n;i++){
             ans += abs((long long)vec[i].first - vec[tar].first) * vec[i].second;
@@ -36,13 +30,3 @@ public:
         
     }
 };
-
-
-
-
-
-
-
-
-
-
