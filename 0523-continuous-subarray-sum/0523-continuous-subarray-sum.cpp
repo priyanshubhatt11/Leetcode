f34020adfc6ab3f3a37b948be80e846f006be90e
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-26 12:53:40 +0530

    Time: 901 ms (5.02%), Space: 113.8 MB (46.74%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,10 +3,11 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int,int> map;
         int sum =0;
-        map[0] = -1;
+        map[0] = -1;    // we take map[0] as -1 test this case -> [2,4,3] k=6
+
         for(int i=0;i<nums.size();i++){
             sum += nums[i];
-              
+            cout<<sum<<" ";
             if(k!= 0)sum = sum % k;
 
             if(map.count(sum)){
