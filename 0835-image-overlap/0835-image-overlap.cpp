d6066cfa4744ced4024ebca7c64a4ae05b5abeb0
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-27 17:26:01 +0530

    Time: 195 ms (44.45%), Space: 9.4 MB (93.75%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    
-    int solve(vector<vector<int>>&img1, vector<vector<int>> &img2, int r, int c){
-        int count =0;
-        int n = img1.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(r+i < 0 || r+i >=n || c+j <0 || c +j >=n)continue;
-                
-                if(img1[i][j] + img2[i+r][j+c] == 2)count++;
-            }
-        }
-        return count;
-    }
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int n = A.size();
         int ans =0;
-        int n = img1.size();
-        for(int i= -n+1 ;i<n;i++){
-            for(int j = -n+1; j<n; j++){
-                ans = max(ans, solve(img1, img2, i, j));
-                //cout<<i<<j<<" ";
+        for(int dx = -n-1; dx < n; dx++){
+            for(int dy = -n-1;dy < n; dy++){
+                int temp =0;
+                for(int x =0; x<n && x + dx < n ;x++){
+                    if(x + dx < 0)continue;
+                    for(int y =0; y< n && y + dy < n; y++){
+                        if(y + dy < 0)continue;
+                        if(A[x][y] + B[x+dx][y+dy] == 2)temp++;
+                    }
+                }
+                ans = max(ans, temp);
             }
-            //cout<<endl;
         }
         return ans;
     }
