5f89b71e60464e9334252cdee496b3a6757e469f
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-29 22:56:11 +0530

    Time: 110 ms (53.59%), Space: 31.4 MB (80.11%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,16 +1,13 @@
 class Solution {
-public:
+public: 
     int firstMissingPositive(vector<int>& nums) {
-        unordered_map<int,int> map;
-        int mi = *min_element(nums.begin(), nums.end());
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            map[nums[i]] = 1;
-        }
-        for(int i=1;i<=n+1;i++){
-            if(map[i] == 0)return i;
-        }
-        
-        return 0;
+        int n = nums.size(); 
+        for (int i = 0; i < n; i++)
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+                swap(nums[i], nums[nums[i] - 1]);
+        for (int i = 0; i < n; i++)
+            if (nums[i] != i + 1)
+                return i + 1;
+        return n + 1;
     }
 };
