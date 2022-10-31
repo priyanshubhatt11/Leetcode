8eaaf42e69e2302a9aa19a6f67103b3369f68e9d
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-10-31 19:25:48 +0530

    Time: 1836 ms (17.02%), Space: 345.7 MB (10.69%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    bool isvalid(string s, unordered_map<string, int> map, int sz){
+        for(int i=0;i<s.size();i += sz){
+            string ss = s.substr(i, sz);
+            map[ss]--;
+            if(map[ss] < 0)return false;
+        }
+        return true;
+    }
+    
+    vector<int> findSubstring(string s, vector<string>& words) {
+        unordered_map<string, int> map;
+        for(string i :words){
+            map[i]++;
+        }
+        
+        int n = s.size(), mx = words.size(), sz = words[0].size();
+        int m = mx * sz;
+        
+        vector<int> ans;
+        for(int i=0; i+m <=n ;i++){
+            //cout<<s.substr(i,m)<<endl;
+            if(isvalid(s.substr(i, m) , map, sz)){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
