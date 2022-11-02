62a290e26933cd018ee3926561c2e3625770acf8
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-02 23:13:51 +0530

    Time: 3 ms (39.91%), Space: 6 MB (16.90%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -14,8 +14,9 @@ public:
         int l=1,r=num/2;
         while(l<=r){
             long long mid = (l+r)/2;
-            if(mid * mid == num)return true;
-            else if(mid * mid > num)    r = mid-1;
+            long long n = mid*mid;
+            if(n == num)return true;
+            else if(n > num)    r = mid-1;
             else l= mid+1;
         }
         return false;
