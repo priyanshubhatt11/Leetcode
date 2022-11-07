e8c03e73ac76b0a92923b67680e000e4cbad2fa9
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-08 00:00:52 +0530

    Time: 3 ms (44.59%), Space: 5.9 MB (27.93%) - LeetHub

---
+++ 0878-nth-magical-number/0878-nth-magical-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    long long findlcm(int a, int b){
+        
+        return (a*b)/__gcd(a,b);
+    }
+    
+    int nthMagicalNumber(int n, int a, int b) {
+        long long l=1, h=1e17, target, mid;
+        
+        long long lcm = findlcm(a,b);
+        
+        while(l<h){
+            mid = (l+h)/2;
+            target = (mid/a) + (mid/b) - (mid/lcm);
+            if(target < n){
+                l = mid+1;
+            }
+            else{
+                h = mid;
+            }
+        }
+        return h%1000000007;
+    }
+};
