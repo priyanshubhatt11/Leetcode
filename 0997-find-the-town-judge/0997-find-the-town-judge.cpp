7e5c4f4675428ddc9d107c32a03b946419aea0fb
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-09 16:17:08 +0530

    Time: 506 ms (10.41%), Space: 68.7 MB (44.43%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<vector<int>> vec(n+1);
-        vector<bool> vis(n+1, false);
+        // vector<vector<int>> vec(n+1);
+        // vector<bool> vis(n+1, false);
+        // for(auto i:trust){
+        //     vec[i[1]].push_back(i[0]);
+        //     vis[i[0]] = true;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     if(vec[i].size() == n-1 && vis[i] == false){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        // 
+        
+        //          OR
+        
+        vector<int> vec(n+1, 0);
         for(auto i:trust){
-            vec[i[1]].push_back(i[0]);
-            vis[i[0]] = true;
+            vec[i[0]]--;
+            vec[i[1]]++;
         }
-        for(int i=1;i<=n;i++){
-            if(vec[i].size() == n-1 && vis[i] == false){
-                return i;
-            }
-        }
+        for(int i=1;i<=n;i++) if(vec[i] == n-1)return i;
         return -1;
+        
     }
 };
