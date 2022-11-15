1e28f89d068303aa0bdccc13b841c9493ef2ce35
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-15 11:17:22 +0530

    Time: 76 ms (20.48%), Space: 30.8 MB (93.85%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -2,17 +2,16 @@
 class Solution {
 public:
     
-    int count =0;
-    
+    int ans= 0;
     void solve(TreeNode*root){
-        if(root==NULL)return;
+        if(root == NULL)return;
+        ans++;
         solve(root->left);
         solve(root->right);
-        count++;
     }
     
     int countNodes(TreeNode* root) {
         solve(root);
-        return count;
+        return ans;
     }
 };
