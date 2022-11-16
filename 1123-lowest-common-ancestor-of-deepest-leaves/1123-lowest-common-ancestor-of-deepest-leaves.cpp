d80bd1225d333be31fd82bda34b8ade12958ee3c
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-16 15:12:29 +0530

    Time: 32 ms (40.25%), Space: 21.2 MB (35.21%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -3,26 +3,16 @@ class Solution {
 public:
     
     int height(TreeNode* root){
-        if(root == NULL)return NULL;
-        return max(1 + height(root->left) , 1+ height(root->right));
-    }
-    
-    TreeNode* solve(TreeNode*root){
-        if(root == NULL)return NULL;
-        
-        if(height(root->left) > height(root->right) ){
-            return solve(root->left);
-        }
-        else if(height(root->left) < height(root->right)){
-            return solve(root->right);
-        }
-        else {
-            return root;
-        }
-       
+        if(root == NULL)return 0;
+        return max(1 + height(root->left), 1 + height(root->right));
     }
     
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        return solve(root);
+        if(root == NULL)return NULL;
+        
+        if(height(root->left) > height(root->right))    return lcaDeepestLeaves(root->left);
+        else if(height(root->left) < height(root->right))   return lcaDeepestLeaves(root->right);
+        else return root;
+        
     }
 };
