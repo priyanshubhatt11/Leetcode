3c0342f9d384bf6e6f46970048e84a2b7a869ab5
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-16 15:24:46 +0530

    Time: 46 ms (8.83%), Space: 23.9 MB (6.42%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -2,9 +2,30 @@
 class Solution {
 public:
     
+//     int height(TreeNode* root){
+//         if(root == NULL)return 0;
+//         return max(1 + height(root->left), 1 + height(root->right));
+//     }
+    
+//     TreeNode* lcaDeepestLeaves(TreeNode* root) {
+//         if(root == NULL)return NULL;
+        
+//         if(height(root->left) > height(root->right))    return lcaDeepestLeaves(root->left);
+//         else if(height(root->left) < height(root->right))   return lcaDeepestLeaves(root->right);
+//         else return root;
+        
+//     }
+    
+//                            OR
+    
+    unordered_map<TreeNode*, int> map;  // deep
+    
     int height(TreeNode* root){
         if(root == NULL)return 0;
-        return max(1 + height(root->left), 1 + height(root->right));
+        if(map.count(root)) return map[root];
+        
+        return map[root] = max(1 + height(root->left), 1+ height(root->right));
+        
     }
     
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
@@ -12,7 +33,8 @@ public:
         
         if(height(root->left) > height(root->right))    return lcaDeepestLeaves(root->left);
         else if(height(root->left) < height(root->right))   return lcaDeepestLeaves(root->right);
-        else return root;
+         return root;
         
+    
     }
 };
