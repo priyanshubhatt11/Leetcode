05644e4e4c8687ada30e408e17fd6c5416ba1eab
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-22 19:02:58 +0530

    Time: 485 ms (31.99%), Space: 79.6 MB (87.87%) - LeetHub

--- 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
+++ 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
@@ -1,19 +1,48 @@
 class Solution {
 public:
+   // int maxNonOverlapping(vector<int>& nums, int target) {
+   //      unordered_map<int,int> map;
+   //      map[0] = 1;
+   //      int ans =0;
+   //      int sum =0;
+   //      for(int i=0;i<nums.size();i++){
+   //          sum += nums[i];
+   //          if(map.find(sum - target) != map.end()){
+   //              ans++;
+   //              sum =0;
+   //              map.clear();
+   //          }
+   //          //cout<<sum<<" ";
+   //          map[sum] = 1;
+   //      }
+   //      return ans;
+   //  }
     int maxNonOverlapping(vector<int>& nums, int target) {
-        unordered_map<int,int> map;
-        map[0] = 1;
-        int ans =0;
-        int sum =0;
+        unordered_set<int> set;
+        set.insert(0);
+        int ans =0, sum =0;
         for(int i=0;i<nums.size();i++){
             sum += nums[i];
-            if(map.find(sum - target) != map.end()){
+            if(set.count(sum - target)){
                 ans++;
                 sum =0;
-                map.clear();
+                set.clear();
             }
-            map[sum] = 1;
+            set.insert(sum);
         }
+        
         return ans;
     }
+    
 };
+
+
+
+
+
+
+
+
+
+
+
