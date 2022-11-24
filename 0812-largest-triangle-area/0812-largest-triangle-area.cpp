1620c96ae47879e86388c7cc40532b944a8cf3f7
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-24 09:33:47 +0530

    Time: 19 ms (71.73%), Space: 7.7 MB (16.74%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.cpp
+++ 0812-largest-triangle-area/0812-largest-triangle-area.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
+    //  area = (1/2)* (x1(y2 - y3) + x2(y3 - y1) + x3(y1 - y2))
+    
     double largestTriangleArea(vector<vector<int>>& points) {
-        //vector<double> v;
-        double mx = 0.0;
-        
-        int i=0;
-        while(i<points.size())
-        {
-          int j=i+1;
-          while(j<points.size())
-          {
-            int k=j+1;
-            while(k<points.size())
-            {
-               //x1(y2-y3)
-               double a=points[i][0]*(points[j][1]-points[k][1]);
-               //x2(y3-y1)
-               double b=points[j][0]*(points[k][1]-points[i][1]);
-               //x3(y1-y2)
-               double c=points[k][0]*(points[i][1]-points[j][1]);
-               double x=0.5;
-               double area=(x)*(abs(a+b+c));
-               //cout<<"area: "<<area<<endl;
-               mx = max(mx , area);
-               k++;
+        int n = points.size();
+        double ans =0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k = j+1;k<n;k++){
+                    double x1 = (double)points[i][0];
+                    double x2 = (double)points[j][0];
+                    double x3 = (double)points[k][0];
+                    double y1 = (double)points[i][1];
+                    double y2 = (double)points[j][1];
+                    double y3 = (double)points[k][1];
+                    
+                    double a = 0.5 * (x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2));
+                    
+                    ans = max(ans , abs(a));
+                    
+                }
             }
-            j++;
-          }
-          i++;
         }
-        // int max=*max_element(v.begin(),v.end());
-         return mx;
-       
+        return ans;
     }
 };
