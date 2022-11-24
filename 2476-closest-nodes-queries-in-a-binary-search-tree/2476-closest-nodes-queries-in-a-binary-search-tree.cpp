cd3e7c3c190e68bbd254b5991c0aa98822487742
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-24 13:53:23 +0530

    Time: 530 ms (69.14%), Space: 177.1 MB (5.03%) - LeetHub

--- 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -1,31 +1,39 @@
+
 class Solution {
 public:
-    vector<int>arr;
     
-    void dfs(TreeNode* root){
-        if(!root) return;
-        
-        dfs(root->left);
-        arr.push_back(root->val);
-        dfs(root->right);
+    void inorder(TreeNode* root, vector<int> &vec){
+        if(root == NULL)return;
+        inorder(root->left, vec);
+        vec.push_back(root->val);
+        inorder(root->right, vec);
     }
-    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
-        dfs(root);
-        vector<vector<int>>ans;
+    
+    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& q) {
+        vector<int> vec;
+        inorder(root, vec);
+        vector<vector<int>> ans;
         
-        for(auto& x : queries){
-            vector<int>t(2,-1);
-            auto it = lower_bound(arr.begin(),arr.end(),x+1);
-            if(it != arr.begin()) {
-                it--;
-                t[0] = *it;
+        //1 2 4 6 9 13 14 15
+        for(auto i:q){
+            vector<int> vv;
+            int idx1 = upper_bound(vec.begin(), vec.end(), i) - vec.begin();
+            idx1--;
+            if(idx1 < 0){
+                vv.push_back(-1);
             }
-            it = lower_bound(arr.begin(),arr.end(),x);
+            else vv.push_back(vec[idx1]);
             
-            if(it != arr.end()) t[1] = *it;
-            ans.push_back(t);    
+            int idx2 = lower_bound(vec.begin(), vec.end(), i) - vec.begin();
+            if(idx2 >= vec.size()){
+                vv.push_back(-1);
+            }
+            else vv.push_back(vec[idx2]);
+            
+            ans.push_back(vv);
+            
         }
-        return ans;
         
+        return ans;
     }
 };
