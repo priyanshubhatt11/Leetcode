1728d27717251b33d1e63e6ef0580ec65eac5eac
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-29 20:14:49 +0530

    Time: 1120 ms (71.64%), Space: 154.7 MB (64.48%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -9,9 +9,13 @@ public:
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 long long diff = (long long) nums[i] - (long long)nums[j];
-                
-                dp[i][diff] += dp[j][diff] + 1;
-                ans += dp[j][diff];
+                int count = 0;
+            
+                if (dp[j].find(diff) != dp[j].end()){
+                    count = dp[j][diff];
+                }
+                dp[i][diff] += count + 1;
+                ans += count;
             }
         }
         return ans;
