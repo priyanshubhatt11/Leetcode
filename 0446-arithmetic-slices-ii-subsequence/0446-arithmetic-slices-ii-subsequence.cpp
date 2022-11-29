6face276f20231b98a3e6f373a81c7024ed3bf70
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-11-29 20:13:36 +0530

    Time: 2524 ms (10.39%), Space: 285.2 MB (19.47%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        // vector<int> slice;
-        // return numberOfArithmeticSlices(nums, 0, slice);
-        int ans = 0, n = nums.size();
-        vector<unordered_map<long, int>> dp(n);
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                int count = 0;
-                long diff = (long)nums[i] - nums[j];
-                if (dp[j].find(diff) != dp[j].end()){
-                    count = dp[j][diff];
-                }
-                dp[i][diff] += count + 1;
-                ans += count;
+        long long ans =0;
+        int n = nums.size();
+        
+        vector<unordered_map<long long , int>> dp(n);
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                long long diff = (long long) nums[i] - (long long)nums[j];
+                
+                dp[i][diff] += dp[j][diff] + 1;
+                ans += dp[j][diff];
             }
         }
         return ans;
-        
     }
 };
