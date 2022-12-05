da86c27faf0e782a6d92a37fed950aff4b2600a8
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2022-12-05 19:46:06 +0530

    Time: 371 ms (11.28%), Space: 91 MB (19.31%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,40 +1,33 @@
-class Solution
-{
-    public:
-        int minimumAverageDifference(vector<int> &nums)
-        {
-
-            int n = nums.size();
-            vector<long long> presum(n, 0), lastsum(n, 0);
-            long long fsum = 0, lsum = 0;
-
-            for (int i = 0; i < n; i++)
-            {
-                fsum += nums[i];
-                presum[i] = fsum;
-                lastsum[n - i - 1] = lsum;
-                lsum += nums[n - i - 1];
-            }
-
-            long long minAD = INT_MAX;
-            int ans = -1;
-            long long tempAD;
-            for (int i = 0; i < n; i++)
-            {
-                if (i == n - 1)
-                {
-                    tempAD = presum[i] / (i + 1);
-                }
-                else
-                {
-                    tempAD = abs(presum[i] / (i + 1) - lastsum[i] / (n - i - 1));
-                }
-                if (tempAD < minAD)
-                {
-                    minAD = tempAD;
-                    ans = i;
-                }
-            }
-            return ans;
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> pref(n,0), suff(n,0);
+        long long s1 =0, s2 =0;
+        for(int i=0;i<n;i++){
+            s1 += nums[i];
+            s2 += nums[n-i-1];
+            pref[i] += s1;
+            suff[n-i-1] = s2;
         }
+        
+        int ans = INT_MAX;
+        int x = INT_MAX;
+        int idx  = -1;
+        for(int i=0;i<n;i++){
+            if(i == n-1){
+                x = pref[i]/(i+1);
+            }
+            else {
+                x = abs(pref[i]/(i+1) - suff[i+1]/(n-i-1));
+            }
+            
+            if(ans > x){
+                ans = x;
+                idx = i;
+            }
+        }
+        return idx;
+        
+    }
 };
