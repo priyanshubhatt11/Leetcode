0195a2cb9f8148f7845a9cd53482fd72b9555bff
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-01-19 23:15:05 +0530

    Time: 19 ms (48.45%), Space: 6 MB (79.13%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     int solve(int n, int val, int paste){
-        if(val > n)return 1e8;
+        if(val > n)return 1001;
         if(val == n)return 0;
         
         return 1 + min(1+solve(n, 2*val, val), solve(n, val+paste, paste));
