64400041e54babcc35c7ee0878be1ce7000135f6
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-01-19 21:29:11 +0530

    Time: 4 ms (73.71%), Space: 9.5 MB (55.69%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,9 +4,7 @@ public:
         int n = mat.size();
         int m = mat[0].size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j] == target)return true;
-            }
+            if(binary_search(mat[i].begin(), mat[i].end() , target))return true;
         }
         return false;
     }
