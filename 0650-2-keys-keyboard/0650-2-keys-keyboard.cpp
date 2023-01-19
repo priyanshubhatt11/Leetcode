16066e96ae38c3cb94b828d11597fb312dd4c58f
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-01-19 23:14:53 +0530

    Time: 17 ms (49.69%), Space: 6 MB (57.76%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    
+    int solve(int n, int val, int paste){
+        if(val > n)return 1e8;
+        if(val == n)return 0;
+        
+        return 1 + min(1+solve(n, 2*val, val), solve(n, val+paste, paste));
+    }
+    
+    int minSteps(int n) {
+        if(n == 1)return 0;
+        return solve(n, 1, 1) + 1;    
+    }
+};
