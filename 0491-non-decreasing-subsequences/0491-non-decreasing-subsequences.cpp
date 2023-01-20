b636b8366f395f89bd31264bb327bb9c810b6ee2
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-01-20 09:36:57 +0530

    Time: 80 ms (66.50%), Space: 22.4 MB (70.61%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -4,23 +4,28 @@ public:
     int n;
     
     void solve(vector<int>&nums, vector<int>&vec, int idx){
-        if(vec.size()>=2)st.insert(vec);
-        
-        for(int i=idx;i<n;i++){
-            if(vec.empty() || vec.back() <= nums[i]){
-                vec.push_back(nums[i]);
-                solve(nums, vec, i+1);
-                vec.pop_back();
-            }
+        if(idx >= n){
+            return;
         }
+        if(vec.back() <= nums[idx]){
+            vec.push_back(nums[idx]);
+            st.insert(vec);
+            solve(nums, vec, idx+1);
+            
+            vec.pop_back();
+            solve(nums, vec, idx+1);
+        }
+        else solve(nums, vec, idx+1);
     }
     
     vector<vector<int>> findSubsequences(vector<int>& nums) {
            
         n = nums.size();
-
-        vector<int> vec;
-        solve(nums, vec, 0);
+        for(int i=0;i<n;i++){
+            vector<int> vec;
+            vec.push_back(nums[i]);
+            solve(nums, vec, i+1);
+        }
         vector<vector<int>> ans(st.begin(), st.end());
         return ans;
         
