86145596518111b2370fee9903dac8f44e11e903
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-02-12 15:27:24 +0530

    Time: 207 ms (49.83%), Space: 13.9 MB (88.00%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int ladderLength(string beg, string end, vector<string>& words) {
-        unordered_map<string ,int> mp;
-        for(auto i:words)mp[i]++;
+        //unordered_map<string ,int> mp;
+        unordered_set<string> st(words.begin(), words.end());
+        //for(auto i:words)mp[i]++;
         
         queue<pair<string,int>> q;
         q.push({beg, 0});
@@ -15,9 +16,9 @@ public:
             for(int i=0;i<s.size();i++){
                 for(char j='a';j<='z';j++){
                     string temp = s.substr(0,i) + j + s.substr(i+1);
-                    if(mp[temp] > 0){
+                    if(st.find(temp) != st.end()){
                         //cout<<temp<<" ";
-                        mp[temp]--;
+                        st.erase(temp);
                         q.push({temp, c+1});
                         
                     }
