acdc21177e6e139b94d0a4456ecdca84b350e2fa
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-02-23 21:14:50 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -21,9 +21,7 @@ class Solution {
 	    for(int k=0;k<n;k++){
 	        for(int i=0;i<n;i++){
 	            for(int j=0;j<n;j++){
-	                if(mat[i][j] > mat[i][k] + mat[k][j]){
-	                    mat[i][j] = mat[i][k] + mat[k][j];
-	                }
+	                mat[i][j] = min(mat[i][j] , mat[i][k] + mat[k][j]);
 	            }
 	        }
 	    }
