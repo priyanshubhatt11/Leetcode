a13389862b518713d32a219b28b2d53aabb96463
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-02-25 11:04:14 +0530

    Time: 2446 ms (19.84%), Space: 14.5 MB (72.85%) - LeetHub

---
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int dp[71][5000];
+    
+    int solve(vector<vector<int>>&mat, int tar, int idx, int num){
+        
+        if(idx >= mat.size()){
+            return abs(tar - num);
+        }
+        if(dp[idx][num] != -1){
+            return dp[idx][num];
+        }
+        int ans =INT_MAX;
+        for(int i=0;i<mat[idx].size();i++){
+            ans = min(ans , solve(mat, tar, idx+1, num + mat[idx][i]));
+        }
+        return dp[idx][num] = ans;
+    }
+    
+    int minimizeTheDifference(vector<vector<int>>& mat, int tar) {
+        memset(dp,-1,sizeof(dp));
+        return solve(mat, tar, 0, 0);
+        
+    }
+};
