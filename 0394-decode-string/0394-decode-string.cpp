cdb6cf1fd217c7fbfc4754dc82532a8966b0d12b
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-02-25 22:57:37 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (56.35%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -7,12 +7,13 @@ public:
         for(;i<s.size();i++){
             if(s[i] >='0' && s[i] <='9') num = num*10 + (s[i]-'0');
             else if(s[i] == '['){
-                string ans = solve(s, ++i);
-                while(num--) str+=ans;
-                num =0;
+                string ans = solve(s, ++i); // ++i because we have done call by reference and with the help of call by reference it will not come to its previous index after return.
+                cout<<num<<" ";
+                while(num--) str+=ans;  
+                num =0; // num become negative so we initialize it to zero so that in next iteration this will not change the value of next num.
             }
             else if(s[i] == ']')return str;
-            else str += s[i];
+            else str += s[i]; // if s[i] is character.
             
         }
         return str;
