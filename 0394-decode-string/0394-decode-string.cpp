ea66ad52ed0e253e2971d5fc39987a60d2623529
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-02-25 23:01:52 +0530

    Time: 2 ms (45.46%), Space: 6.6 MB (56.35%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    // while we not getting closing bracket we add string to temp string and when we get it we return it and append its num times in original string
     string solve(string s, int &i){
         int num =0;
         string str = "";
@@ -8,7 +8,7 @@ public:
             if(s[i] >='0' && s[i] <='9') num = num*10 + (s[i]-'0');
             else if(s[i] == '['){
                 string ans = solve(s, ++i); // ++i because we have done call by reference and with the help of call by reference it will not come to its previous index after return.
-                                while(num--) str+=ans;  
+                while(num--) str+=ans;  
                 num =0; // num become negative so we initialize it to zero so that in next iteration this will not change the value of next num.
             }
             else if(s[i] == ']')return str;
