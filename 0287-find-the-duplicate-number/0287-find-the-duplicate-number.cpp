b082e131634920114f2120d7d33b4c3c01c447f9
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-03-11 20:00:11 +0530

    Time: 119 ms (67.28%), Space: 61.3 MB (67.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,20 +1,14 @@
-// Floyd cycle detection
-// similar to cycle detection in linked list
-
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[0];
+        int slow = nums[0], fast = nums[0];
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
         }while(slow!=fast);
         
-        //return slow;    // returning slow pointer here only work for two duplicate element
-        
         slow = nums[0];
-        while(slow != fast){
+        while(slow!=fast){
             slow = nums[slow];
             fast = nums[fast];
         }
