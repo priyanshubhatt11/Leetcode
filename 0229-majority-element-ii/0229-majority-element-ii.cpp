79da100da89a2a2198d5a3ea688c4f403c80d841
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-03-14 15:52:51 +0530

    Time: 13 ms (70.87%), Space: 16 MB (60.69%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,33 +1,32 @@
-//  Boyre Moore Voting Algo
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int num1=-1, num2=-1, c1=0,c2=0;
-        for(int i=0;i<nums.size();i++){
-            if(num1 == nums[i])c1++;
-            else if(num2 == nums[i])c2++;
+        int nums1 = -1, nums2 = -1, c1=0, c2 =0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(nums1 == nums[i])c1++;
+            else if(nums2 == nums[i]) c2++;
             else if(c1 == 0){
-                num1 = nums[i];
+                nums1 = nums[i];
                 c1++;
             }
             else if(c2 == 0){
-                num2 = nums[i];
+                nums2 = nums[i];
                 c2++;
             }
-            else {
+            else{
                 c1--;
                 c2--;
             }
         }
-        c1=0,c2=0;
-        for(auto i:nums){
-            if(i == num1)c1++;
-            else if(i==num2)c2++;
+        c1 =0, c2=0;
+        for(int i:nums){
+            if(i == nums1)c1++;
+            else if(i == nums2)c2++;
         }
-        int n = nums.size();
-        vector<int> ans;
-        if(c1 > n/3)ans.push_back(num1);
-        if(c2 > n/3)ans.push_back(num2);
-        return ans;
+        vector<int> vec;
+        if(c1 > n/3)vec.push_back(nums1);
+        if(c2 > n/3)vec.push_back(nums2);
+        return vec;
     }
 };
