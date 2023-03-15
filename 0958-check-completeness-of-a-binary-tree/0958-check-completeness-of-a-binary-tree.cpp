30648773c125b6a72c65d8a0add1b89161e36351
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-03-15 12:12:15 +0530

    Time: 10 ms (19.69%), Space: 10.6 MB (29.89%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -1,43 +1,31 @@
 
 class Solution {
 public:
-    
-    int height(TreeNode*root){
-        if(root == NULL)return 0;
-        return max(1+height(root->left), 1+ height(root->right));
-    }
     bool isCompleteTree(TreeNode* root) {
-        int h = height(root);
+        
         
         queue<TreeNode*> q;
         q.push(root);
-        int c =0;
+        bool flag = false;
         while(!q.empty()){
             int k = q.size();
-            if(pow(2, c) != k)return false;
-            if(c==h-2)break;
-           
-            c++;
+            
             while(k--){
                 TreeNode* temp = q.front();
                 q.pop();
+                
+               
+                
+                if(!flag && (temp->left == NULL || temp->right == NULL))flag = true;
+                else if(flag && (temp->left != NULL || temp->right != NULL))return false;
+                if(temp->left == NULL && temp->right != NULL)return false;
+                    
                 if(temp->left)q.push(temp->left);
                 if(temp->right)q.push(temp->right);
             }
             
         }
-        int k = q.size();
-        
-        bool flag = false, ff = false, fd= false;
-        while(!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            
-            if(!flag && temp->right == NULL)flag = true;
-            else if(temp->right != NULL && temp->left == NULL)return false;
-            else if(flag && (temp->left != NULL || temp->right != NULL))return false;
-            
-        }
+   
         
         return true;
     }
