6e71591ceecde7fc263715ed1e08dc76376e7273
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-03-17 10:37:55 +0530

    Time: 28 ms (37.07%), Space: 19.7 MB (49.43%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -3,8 +3,8 @@ public:
     TreeNode* recoverFromPreorder(string s) {
         unordered_map<int, TreeNode*> mp;
         
-        int j=0;
-        int v =0;
+         int j=0;
+        // int v =0;
         // while(j<s.size() && s[j] >='0' && s[j] <='9'){
         //     v = v*10 + (s[j] -'0');
         //     j++;
@@ -14,25 +14,20 @@ public:
         
         int n = s.size();
         for(int i=j;i<s.size();){
-            bool f1=false, f2=false;
-            int c =0;
-            string num = "";
+            int c =0, num = 0;
             while(i<s.size() && s[i] == '-'){
                 c++;
-                f1=true;
                 i++;
             }
             
             while(i<s.size() && s[i] >='0' && s[i]<='9'){
-                num.push_back(s[i]);
+                num = num*10 + (s[i] - '0');
                 i++;
-                f2=true;
             }
             
-            int x = stoi(num);
-            mp[c] = new TreeNode(x);
+            mp[c] = new TreeNode(num);
             //cout<<c<<" "<<x<<endl;
-            if(c>0){
+            if(c>0){    // their is no c-1 node of root node
                 if(mp[c-1]->left == NULL){
                     mp[c-1]->left = mp[c];
                 }
