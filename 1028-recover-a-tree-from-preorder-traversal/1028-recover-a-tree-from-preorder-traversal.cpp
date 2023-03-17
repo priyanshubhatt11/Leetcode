fca4dc3786bfb5014ecf01a8d0103a5fd2211fcd
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-03-17 10:35:21 +0530

    Time: 28 ms (37.07%), Space: 19.5 MB (77.59%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -5,12 +5,12 @@ public:
         
         int j=0;
         int v =0;
-        while(j<s.size() && s[j] >='0' && s[j] <='9'){
-            v = v*10 + (s[j] -'0');
-            j++;
-        }
+        // while(j<s.size() && s[j] >='0' && s[j] <='9'){
+        //     v = v*10 + (s[j] -'0');
+        //     j++;
+        // }
         
-        mp[0] = new TreeNode(v);
+        //mp[0] = new TreeNode(v);
         
         int n = s.size();
         for(int i=j;i<s.size();){
@@ -32,10 +32,13 @@ public:
             int x = stoi(num);
             mp[c] = new TreeNode(x);
             //cout<<c<<" "<<x<<endl;
-            if(mp[c-1]->left == NULL){
-                mp[c-1]->left = mp[c];
+            if(c>0){
+                if(mp[c-1]->left == NULL){
+                    mp[c-1]->left = mp[c];
+                }
+                else mp[c-1]->right = mp[c];
             }
-            else mp[c-1]->right = mp[c];
+            
             
         }
         return mp[0];
