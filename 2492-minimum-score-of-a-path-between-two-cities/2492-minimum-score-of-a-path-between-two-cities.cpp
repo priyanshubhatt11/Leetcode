17ac9599c200b1e4f28ace18b55d61332f502119
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-03-22 23:30:01 +0530

    Time: 539 ms (48.31%), Space: 134.8 MB (52.62%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int ans = INT_MAX;
+    
+    void solve(vector<pair<int,int>> vec[], vector<bool>&vis, int idx){
+        
+        // ans = min(ans, vec[idx].second);
+        vis[idx] = true;
+        for(auto i:vec[idx]){
+            ans = min(ans, i.second);
+            if(!vis[i.first]){
+                solve(vec, vis, i.first);
+            }
+        }
+    }
+    
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<pair<int,int>> vec[n+1];
+        for(auto i:roads){
+            vec[i[0]].push_back({i[1], i[2]});
+            vec[i[1]].push_back({i[0], i[2]});
+        }
+        
+        vector<bool> vis(n+1, false);
+        solve(vec, vis, 1);
+        return ans;
+        
+    }
+};
