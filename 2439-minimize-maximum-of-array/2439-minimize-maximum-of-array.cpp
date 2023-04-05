0cfe0e1f91dcbefa5fd21432f068f8ec7724f5b7
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-04-05 11:15:40 +0530

    Time: 211 ms (29.60%), Space: 71.2 MB (90.47%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    bool ok(vector<int>&nums, int x){
+        long long sum =0;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i] > x){
+                sum += nums[i] - x;
+            }
+            else{
+                sum -= min(sum, (long long)x-nums[i]);
+            }
+        }
+        if(sum == 0)return true;
+        return false;
+    }
+    
+    int minimizeArrayValue(vector<int>& nums) {
+        int l = 0, h = *max_element(nums.begin(), nums.end());
+        int ans =0;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            
+            if(ok(nums, mid)){
+                ans = mid;
+                h = mid-1;
+            }
+            else l = mid+1;
+        }
+        return ans;
+    }
+};
