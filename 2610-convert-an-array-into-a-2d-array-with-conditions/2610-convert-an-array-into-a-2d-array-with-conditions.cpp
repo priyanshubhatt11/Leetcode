f26196be4c02ddfd3a3271e996714cef53f877ec
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-04-11 09:55:26 +0530

    Time: 23 ms (29.95%), Space: 29.9 MB (57.16%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,27 +1,36 @@
-class Solution {
-public:
-    vector<vector<int>> findMatrix(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int mx=0, ans =0;
-        for(auto i:nums)mp[i]++;
-        for(auto i:mp){
-            mx = max(mx, i.second);
-        }
-        
-        vector<vector<int>> vec(mx);
-        
-        int c =0;
-        while(true){
-            for(auto &j:mp){
-                if(j.second > 0){
-                    mp[j.first]--;
-                    vec[c].push_back(j.first);
-                }
+class Solution
+{
+    public:
+        vector<vector < int>> findMatrix(vector<int> &nums)
+        {
+            vector<vector < int>> ans;
+            unordered_map<int, int> mp;
+            for (auto elem: nums)
+            {
+                mp[elem]++;
             }
-            c++;
-            if(c == mx)break;
+            // goes until the map size is down to 0 
+            
+            while (mp.size() != 0)
+            {
+                vector<int> need;
+
+                for (auto &elem: mp)
+                {
+                    //cout<<elem.first<<" ";
+                                    // pushing only one elem to the vector and decrease the map value count
+                    if(elem.second <= 0)continue;
+                    need.push_back(elem.first);
+                    mp[elem.first]--;
+                    //if(mp[elem.first] == 0)mp.erase(elem.first);
+                    
+                }
+
+                                    
+                if(need.size() == 0)break;
+
+                ans.push_back(need);
+            }
+            return ans;
         }
-        return vec;
-        
-    }
 };
