f1fa7d5908449025943899c9e2bdfd9f83ef4141
priyanshubhatt11 <70811514+priyanshubhatt11@users.noreply.github.com>
2023-12-28 22:03:12 +0530

    Time: 415 ms (5.19%), Space: 106.9 MB (34.02%) - LeetHub

---
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -0,0 +1,54 @@
+class RandomizedCollection {
+public:
+    
+    unordered_map<int, set<int>> mp;
+    vector<int> vec;
+    
+    RandomizedCollection() {
+        
+    }
+    
+    bool insert(int val) {
+        vec.push_back(val);
+        mp[val].insert(vec.size()-1);
+        if(mp[val].size() == 1) return true;
+        return false;
+    }
+    
+    bool remove(int val) {
+        
+        if(mp.find(val) == mp.end())return false;
+        // for(auto i:vec)cout<<i<<" ";
+        // cout<<val;
+        auto st = mp[val];
+        int idx = *st.rbegin();
+        int temp = vec.back();
+        auto st2 = mp[temp];
+        int idx2 = *st2.rbegin();
+        if(val == temp){
+            mp[val].erase(*st.rbegin());
+            
+        }
+        else{
+            vec[idx] = temp;
+            
+            mp[val].erase(idx);
+            mp[temp].erase(idx2);
+            mp[temp].insert(idx);
+        }
+        
+
+        vec.pop_back();
+        if(mp[val].size() == 0){
+            mp.erase(val);
+        }
+        
+        return true;
+    }
+    
+    int getRandom() {
+        int x = vec.size();
+        int n = rand()%x;
+        return vec[n];
+    }
+};
